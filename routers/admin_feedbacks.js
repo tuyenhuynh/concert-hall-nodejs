7f0a92a5d7756131d15cf0addd4514d6f2d1d499
=tuyenhm <tuyenhm93@gmail.com>
2016-08-17 19:06:11 +0300

    Added implementation for module feedback

--- routers/admin_feedbacks.js
+++ routers/admin_feedbacks.js
@@ -3,3 +3,38 @@ var router = express.Router();
 var Feedback = require('../models/feedback');
 
 module.exports = router; 
+
+router.get('/', function(req, res) {
+    Feedback.find({}).exec(function(err, feedbacks) {
+        if(err) {
+            res.send(err); 
+        }else {
+            res.render('../views/admin/feedbacks/index.jade', {title: 'User Feedback', feedbacks}); 
+        }
+    }); 
+}); 
+
+router.route('/:id')
+.get(function(req, res) {
+    var id = req.params.id; 
+    Feedback.find({_id: id}).exec(function(err, feedbacks) {
+        if(err) {
+            console.log('Feed back not found ', id);  
+            res.redirect(req.baseUrl + '/'); 
+        }else {
+            var feedback = feedbacks[0]; 
+            res.render('../views/admin/feedbacks/show.jade', {title: 'Feedback', feedback}); 
+        }
+    });   
+}).post(function(req, res) {
+    //remove feedback
+    var id = req.params.id; 
+    Feedback.findOneAndRemove({_id: id}, function(err, result) {
+        if(err) {
+            console.log(err); 
+        }else {
+            console.log("Removed record with id "  + id); 
+        }
+        res.redirect(req.baseUrl + "/");         
+    }); 
+}); 
